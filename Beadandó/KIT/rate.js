3bc5c6dce9bb526a4b839929259fd95bc5610f79
Kovács István Tamás <ordaska02@gmail.com>
2024-04-28 19:51:28 +0200

    Prog

--- Beadandó/KIT/rate.js
+++ Beadandó/KIT/rate.js
@@ -1,32 +1,7 @@
 $(document).ready(function() {
-    $('#save-ratings-btn').click(function() {
-        var finom = parseInt($('#finom').val());
-        var elkeszitheto = parseInt($('#elkeszitheto').val());
-        var dragasag = parseInt($('#dragasag').val());
-        var megjegyzes = $('#megjegyzes').val();
-        var etel = getUrlParameter('etel');
-
-        var ertekeles = {
-            finom: finom,
-            elkeszitheto: elkeszitheto,
-            dragasag: dragasag,
-            megjegyzes: megjegyzes
-        };
-
-        var ertekelesek = localStorage.getItem(etel) ? JSON.parse(localStorage.getItem(etel)) : [];
-
-        ertekelesek.push(ertekeles);
-
-        localStorage.setItem(etel, JSON.stringify(ertekelesek));
-
-        alert('Az értékelés sikeresen mentve.');
+    $('#save-ratings-btn').click(function(e) {
+        e.preventDefault();
+        alert("Köszönjük az értékelést!");
         window.location.href = "receptek.html";
     });
-
-    function getUrlParameter(name) {
-        name = name.replace(/[\[]/, '\\[').replace(/[\]]/, '\\]');
-        var regex = new RegExp('[\\?&]' + name + '=([^&#]*)');
-        var results = regex.exec(location.search);
-        return results === null ? '' : decodeURIComponent(results[1].replace(/\+/g, ' '));
-    };
 });
