9c0cc026802478e275ccdc91503ded13aac3b264
Kovács István Tamás <ordaska02@gmail.com>
2024-05-05 22:28:49 +0200

    Pótlás

--- AUYLIN_0408/script.js
+++
@@ -1,5 +0,0 @@
-//Fel1
-const txt = '{"név":"Laci", "irányítószám":3704, "város":"Miskolc", "egyetem":"Miskolci Egyetem"}';
-const obj = JSON.parse(txt);
-
-document.getElementById("nev").innerHTML = obj.név +" "+ obj.irányítószám +" "+ obj.város +" "+ obj.egyetem;
