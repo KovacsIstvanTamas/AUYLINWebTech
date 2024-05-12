b8408e894c884fbf5ae89a4f73ef09463bbcb652
Kovács István Tamás <ordaska02@gmail.com>
2024-05-12 09:34:56 +0200

    Legyél jó

---
+++ Beadandó/KIT/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(400).sendFile(path.join(__dirname, 'public', '400.html'));
+});
+
+app.listen(port, () => {
+  console.log(`A szerver fut a http://localhost:${port} címen.`);
+});
